31c63e8304bc3b8f2e2d081db4cd3e0ef7b786ba
VyNguyen <nguyenngocthaovy451999@gmail.com>
2025-07-31 01:06:00 +0700

    update

--- REFERRAL.sql
+++ REFERRAL.sql
@@ -15,3 +15,22 @@ INSERT INTO Referral_Users VALUES
 (5, 'Eve', 2),
 (6, 'Frank', 3),
 (7, 'Grace', 6);
+
+SELECT * FROM Referral_Users
+
+-- Lấy toàn bộ referral của nhân sự gốc Alice
+WITH Referral_hierachy AS (
+SELECT user_id, user_name, referral_id, user_id AS root_id
+FROM Referral_Users
+WHERE user_id = '1'
+UNION ALL
+SELECT a.User_ID, a.User_name, a.Referral_ID, b.root_id
+FROM Referral_Users a
+JOIN referral_hierachy b ON a.Referral_ID = b.user_id
+)
+SELECT * FROM Referral_hierachy
+
+-- Ai giới thiệu ai
+SELECT a.user_id, a.user_name, a.referral_id, b.User_name Referrals_name
+FROM Referral_Users a
+LEFT JOIN (SELECT * FROM Referral_Users) b ON a.Referral_ID = b.User_ID
